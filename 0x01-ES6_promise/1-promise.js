73e10271436a28dd5dd438bda448f4cce76d5a0e
chima-01 <divinechima76@gmail.com>
2024-10-03 10:39:32 -0700

    changed module tyo implicitly check if parameter is a boolean

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,12 @@
 export default function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success === true) {
-      resolve({ status: 200, body: 'succes' });
-    } else if (false) {
-      reject(new Error('The fake API is not working currently'));
-    }
-  });
+  if (typeof success === 'boolean') {
+    return new Promise((resolve, reject) => {
+      if (success) {
+        resolve({ status: 200, body: 'succes' });
+      } else {
+        reject(new Error('The fake API is not working currently'));
+      }
+    });
+  }
+  return Promise.reject(new Error('The fake API is not working currently'));
 }
