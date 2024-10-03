7a4b5ac823ced3731314bc84394467116296b18e
chima-01 <divinechima76@gmail.com>
2024-10-03 11:24:17 -0700

    fixed spelling error

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,9 @@
 export default function getFullResponseFromAPI(success) {
-  if (typeof success === 'boolean') {
-    return new Promise((resolve, reject) => {
-      if (success) {
-        resolve({ status: 200, body: 'succes' });
-      } else {
-        reject(new Error('The fake API is not working currently'));
-      }
-    });
-  }
-  return Promise.reject(new Error('The fake API is not working currently'));
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Succes' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
 }
