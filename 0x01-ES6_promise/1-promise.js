ff62a284ae2b552a9d4331f7723b1ab2b95ea33d
chima-01 <divinechima76@gmail.com>
2024-10-03 10:20:27 -0700

    resolve and reject promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -2,7 +2,7 @@ export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success === true) {
       resolve({ status: 200, body: 'succes' });
-    } else {
+    } else if (false) {
       reject(new Error('The fake API is not working currently'));
     }
   });
