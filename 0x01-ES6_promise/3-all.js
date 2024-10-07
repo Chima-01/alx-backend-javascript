2ffbdb74b6e9c7555ff97feba08a8616b786f790
chima-01 <divinechima76@gmail.com>
2024-10-07 10:20:28 -0700

    refactore code to capture reject

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -6,5 +6,5 @@ export default function handleProfileSignup() {
       const [body, firstName, lastName] = [results[0].body,
         results[1].firstName, results[1].lastName];
       console.log(`${body} ${firstName} ${lastName}`);
-    }, (() => console.error('Signup system offline')));
+    }).catch(() => console.log('Signup system offline'));
 }
