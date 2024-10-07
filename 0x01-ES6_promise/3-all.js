0745425374fad259548afbcd4521232501f0ac8a
chima-01 <divinechima76@gmail.com>
2024-10-07 09:53:48 -0700

    hadling multiple promise

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
+      const [body, firstName, lastName] = [results[0].body,
+        results[1].firstName, results[1].lastName];
+      console.log(`${body} ${firstName} ${lastName}`);
+    }).catch(() => console.error('Signup system offline'));
+}
