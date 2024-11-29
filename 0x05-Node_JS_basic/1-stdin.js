309cccf185182e5c6cf8d8a9334ceeece17cf344
chima-01 <divinechima76@gmail.com>
2024-11-29 14:55:10 +0100

    fix bug in file

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,9 +3,9 @@ const { stdin: input, stdout: output } = require('process');
 
 const data = readline.createInterface({ input, output });
 data.question('Welcome to Holberton School, what is your name?\n', (answer) => {
-  console.log(`Your name is: ${answer}`);
-  if (!process.stdin.isTTY) {
-    console.log('This important software is now closing');
+  output.write(`Your name is: ${answer}\n`);
+  if (!input.isTTY) {
+    output.write('This important software is now closing\n');
   }
   data.close();
 });
