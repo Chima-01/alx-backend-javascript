1198f7fdac51e1bf19915e34832f2c54951b51fd
chima-01 <divinechima76@gmail.com>
2024-11-29 16:42:47 +0100

    use the process module

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,11 @@
-const readline = require('readline');
 const { stdin: input, stdout: output } = require('process');
 
-const data = readline.createInterface({ input, output });
-data.question('Welcome to Holberton School, what is your name?\n', (answer) => {
-  output.write(`Your name is: ${answer}\n`);
-  if (!input.isTTY) {
-    output.write('This important software is now closing\n');
-  }
-  data.close();
+output.write('Welcome to Holberton School, what is your name?\n');
+
+input.on('data', (data) => {
+  output.write(`Your name is: ${data}`);
+});
+
+input.on('end', () => {
+  output.write('This important software is now closing\n');
 });
