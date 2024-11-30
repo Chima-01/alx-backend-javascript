18b43aafe93211be52923a092b898af51afd109f
chima-01 <divinechima76@gmail.com>
2024-11-30 05:49:15 +0100

    ascryn

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    try {
+      const database = fs.readFileSync(path, 'utf-8');
+      const rawData = database.split('\n');
+
+      const info = {
+        total_students: (rawData.length - 2),
+      };
+
+      rawData.forEach((value, index) => {
+        if (index > 0 && value !== '') {
+          const [firstName, ...rest] = value.split(',');
+          const field = rest[rest.length - 1];
+
+          if (!info[field]) {
+            info[field] = [];
+          }
+          info[field].push(firstName);
+        }
+      });
+
+      console.log(`Number of students: ${info.total_students}`);
+
+      for (const key in info) {
+        if (key !== 'total_students') {
+          console.log(`Number of students in ${key}: ${info[key].length}. List: ${info[key].join(', ')}`);
+        }
+      }
+      resolve(true);
+    } catch (error) {
+      reject(new Error('Cannot load the database'));
+    }
+  });
+}
+module.exports = countStudents;
